e2e4454e8f3e986d01208b2528edce4992127427
myemcu <1142497680@qq.com>
2016-08-03 17:00:25 +0800

    V3，子线程发送消息到主线程实现动态更新随机数界面。

--- app/src/main/java/com/myemcu/app_6handler/MainActivity.java
+++ app/src/main/java/com/myemcu/app_6handler/MainActivity.java
@@ -1,12 +1,16 @@
 package com.myemcu.app_6handler;
 
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView mTxt; // 抽取出的对象。
+    // 抽取出的对象。
+    private TextView mTxt;
+    private Handler  mMyHandler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,17 +18,28 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mTxt = (TextView) findViewById(R.id.txt);
-        mTxt.setText("生成的随机数为：\n"+Math.random());
 
-        // 新启动一个线程，打算实现动态更新主线程MainActivity中的随机数。
+        // 创建Habdler对象
+        mMyHandler = new Handler() {
+            public void handleMessage(Message msg) {
+                super.handleMessage(msg);
+
+                if (msg.what==0x12) {
+                    mTxt.setText("生成的随机数为：\n"+Math.random());
+                }
+            }
+        };
+
+        // 新启动一个线程(即:子线程)，打算实现动态更新主线程MainActivity中的随机数。
         new Thread(new Runnable() {
            @Override
            public void run() {
                try {
                     while (true) {
-                        Thread.sleep(300);              // 程序休眠0.3s
-                        Double random = Math.random();  // 生成随机数
-                        mTxt.setText("生成的随机数为："+random);
+                        Thread.sleep(500);              // 线程延时0.5s
+                        Message msg = new Message();    // 建立消息
+                        msg.what=0x12;                  // 标记消息
+                        mMyHandler.sendMessage(msg);    // 发送消息
                     }
                }
                catch (Exception e) {
@@ -32,20 +47,6 @@ public class MainActivity extends AppCompatActivity {
                }
            }
        }).start();
-
-        //-主线程自己刷新随机数---------------------------------------------------------------------------
-        //仅控制台能刷新而界面文本则不能
-        try {
-                for (int i=0;i<5;i++) {
-                    Thread.sleep(300); // 线程休眠0.3s
-                    Double random = Math.random();
-                    System.out.println(random); // 生成控制台打印
-                    mTxt.setText("生成的随机数为："+random);
-                }
-        }
-        catch (Exception e) {
-            e.printStackTrace();
-        }
 
     }
 
